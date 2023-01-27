6cfc22a68aa5221a713eb94b5016eb97a17a2188
Steven Justin <steven@edovo.com>
2023-01-26 21:11:13 -0600

    Typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import {jsx} from '@emotion/react';
+import JSX = jsx.JSX;
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Hello.
+      </header>
+    </div>
+  );
+}
+
+export default App;
